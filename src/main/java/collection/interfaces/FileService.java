60780c92e6fd089cad3afe6ed98d7490374e042b
charlotte <charlotte@codingame.com>
2017-02-22 23:33:32 +0100

    initial commit

---
+++ src/main/java/collection/interfaces/FileService.java
@@ -0,0 +1,35 @@
+package collection.interfaces;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Optional;
+import java.util.stream.Stream;
+
+/**
+ * Created by charlotte on 22/02/17.
+ */
+public class FileService {
+
+    public Optional<String> readFile(URI url){
+        Optional<String> value = Optional.empty() ;
+        try (Stream<String> stream = Files.lines(Paths.get(url))) {
+
+            value = stream.findAny();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return value;
+    }
+
+    public void writeFile(URI url, String valueToWrite) throws IOException, URISyntaxException {
+        Files.write(Paths.get(url), valueToWrite.getBytes());
+        Files.write(Paths.get(url), valueToWrite.getBytes());
+    }
+
+
+
+}
---
+++ src/main/java/collection/interfaces/InsertExercise.java
@@ -0,0 +1,18 @@
+package collection.interfaces;
+
+import java.util.Collection;
+
+public class InsertExercise {
+
+    /**
+     * You must return the collection c enhanced by the element e
+     * @param e
+     * @param c
+     * @return the collection c with the element e
+     */
+    public Collection<String> insertElem(String e, Collection<String> c){
+        // TODO
+        return c;
+    }
+}
+
