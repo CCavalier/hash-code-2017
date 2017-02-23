ec22b2f6f5d427b51190c8489fd0bbc889ff240c
charlotte <charlotte@codingame.com>
2017-02-23 19:19:34 +0100

    first datamodel

--- src/main/java/collection/interfaces/App.java
+++ src/main/java/collection/interfaces/App.java
@@ -1,13 +1,28 @@
 package collection.interfaces;
 
+import java.util.List;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
+
+    public static List<Integer> videoSize;
+
+    public static List<Integer> videoAsked;
+
+    public static List<Integer> cacheSize;
+
+    public static List<Endpoint> endpoints;
+
+
     public static void main( String[] args )
     {
+
+
+
         System.out.println( "Hello World!" );
     }
 }
---
+++ src/main/java/collection/interfaces/Endpoint.java
@@ -0,0 +1,11 @@
+package collection.interfaces;
+
+/**
+ * Created by charlotte on 23/02/17.
+ */
+public class Endpoint {
+
+    Map<Integer, Integer> cacheToLatency;
+
+    Map<Integer, Integer> videoToRequests;
+}
