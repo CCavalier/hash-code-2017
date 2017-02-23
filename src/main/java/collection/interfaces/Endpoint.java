33d84cbf02ea70286495d6bd1fa05cc1124baade
charlotte <charlotte@codingame.com>
2017-02-23 19:48:19 +0100

    endpoint datacenter latency

--- src/main/java/collection/interfaces/Endpoint.java
+++ src/main/java/collection/interfaces/Endpoint.java
@@ -1,9 +1,13 @@
 package collection.interfaces;
 
+import java.util.Map;
+
 /**
  * Created by charlotte on 23/02/17.
  */
 public class Endpoint {
+
+    int dtacenterLatency= 0;
 
     Map<Integer, Integer> cacheToLatency;
 
